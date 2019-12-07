0261261bcb834f3f52d8bf98b4e8dc066a0bc399
NineSwordsMonster <wangjia_1919@163.com>
2019-12-08 00:05:20 +0800

    init

---
+++ index.js
@@ -0,0 +1,14 @@
+IntelliJ IDEAPhpStormWebStorm
+const { path } = require('@vuepress/shared-utils')
+
+module.exports = (options, context) => ({
+  define () {
+    return {
+      COMMENTS_OPTIONS: options || null
+    }
+  },
+  name: '@nines/vuepress-plugin-comments',
+  enhanceAppFiles: [
+    path.resolve(__dirname, './bin/enhanceAppFile.js')
+  ]
+})
